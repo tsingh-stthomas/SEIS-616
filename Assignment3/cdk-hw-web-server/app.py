d740229d01dd4aab575b2195e80f8be51a144167
tsingh-stthomas <159724928+tsingh-stthomas@users.noreply.github.com>
2024-03-09 17:17:43 -0600

    Update app.py

--- Assignment3/cdk-hw-web-server/app.py
+++ Assignment3/cdk-hw-web-server/app.py
@@ -12,6 +12,6 @@ app = cdk.App()
 network_stack = CdkHwNetworkStack(app, "CdkHwNetworkStack")
 
 # Create the Web Server Stack and pass the VPC from the Network Stack
-web_server_stack = CdkHwWebServerStack(app, "CdkHwWebServerStack")
+web_server_stack = CdkHwWebServerStack(app, "CdkHwWebServerStack", cdk_hw_vpc = network_stack.cdk_hw_vpc)
 
 app.synth()
